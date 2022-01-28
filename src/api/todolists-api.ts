577bd43115a8770a5ff611a54abc3d7525661b73
Aliaksandr Rudyk <dezo103@gmail.com>
2022-01-28 19:10:22 +0300

    add fetch todolist loading...

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '8f2534e2-22a4-4052-894e-a66c04807482'
+        'API-KEY': '166b9e97-d8fe-44b6-af0e-8586f7357111'
     }
 })
 
