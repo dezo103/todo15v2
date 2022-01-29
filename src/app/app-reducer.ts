ebb4df105d8f3251201d4e7d6ac4bf0ac5b1c4d3
Aliaksandr Rudyk <dezo103@gmail.com>
2022-01-29 16:50:21 +0300

    add controlled ErrorSnackbar

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,7 +1,10 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+export type ErrorType = string | null
 
 const initialState = {
-    status: 'loading' as RequestStatusType
+    status: 'loading' as RequestStatusType,
+    //error: null as string | null
+    error: 'ERROR'
 }
 
 type InitialStateType = typeof initialState
@@ -10,6 +13,8 @@ export const appReducer = (state: InitialStateType = initialState, action: Actio
     switch (action.type) {
         case 'APP/SET-STATUS':
             return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
         default:
             return state
     }
@@ -24,4 +29,9 @@ export const setAppStatusAC = (status: RequestStatusType) => {
     } as const
 }
 
+export const setAppErrorAC = (error: ErrorType) => ({type: 'APP/SET-ERROR', error} as const)
+
 export type SetAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type SetAppStatusErrorType = ReturnType<typeof setAppErrorAC>
+
+type AppActionsType = SetAppStatusActionType | SetAppStatusErrorType
